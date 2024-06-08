6c7b40185db46b2c6ad40dcefa24eb46b608f3e0
chuni-aria <sonodaumi12402@gmail.com>
2024-06-08 17:23:55 +0800

    Update config.py

--- project/config.py
+++ project/config.py
@@ -6,5 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # LINE Bot 設定
-LINE_CHANNEL_SECRET = os.environ.get("LINE_CHANNEL_SECRET")
-LINE_CHANNEL_ACCESS_TOKEN = os.environ.get("LINE_CHANNEL_ACCESS_TOKEN")
+LINE_CHANNEL_SECRET = os.environ.get("527df324216858c4bc8ef8cc20fe8652")
+LINE_CHANNEL_ACCESS_TOKEN = os.environ.get("fzUW9//V6lYTsVqhjD5GWFGbr55irneEG4XOEBxsCdKD/xQ652hmNtBOooXXFRKnUYhM/qeManMK380gtIqA0J/MfxTRyzc5uCLoOO8iwDpIiwbp2tSfDN0kz86ZV3wAzrhTJrh+nzApSaKUTEBJagdB04t89/1O/w1cDnyilFU=")
+GOOGLE_API_KEY = os.environ.get('AIzaSyBbrZXqpQC1HYIoRZLcug9j3oUwQI78yvw')
+FIREBASE_URL = os.environ.get('https://mazda-ead30-default-rtdb.firebaseio.com/')
